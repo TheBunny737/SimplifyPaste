af111b1261fd0329dcf686de498d8a59896f5e8e
TheBunny737 <teerapat.d@ku.th>
2023-12-05 23:38:05 +0700

    finished frontend test

---
+++ pastebin-frontend/cypress/e2e/pastingComponent.cy.js
@@ -0,0 +1,42 @@
+describe('PastingComponent', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should loads the PastingComponent', () => {
+    cy.contains('button', 'Paste'); // Check if the Paste button exists
+  });
+
+  it('should verifies text on view page', () => {
+    // Type text into the textarea
+    const enteredText = 'Sample text for pasting';
+    cy.get('textarea').type(enteredText);
+
+    // Click the Paste button and navigate to the view page
+    cy.contains('button', 'Paste').click();
+    cy.url().should('include', '/view/'); // Navigate to the view page
+
+    // Verify if the entered text is displayed on the view page
+    cy.get('.paste-text').should('contain', enteredText); // Replace '.paste-text' with your actual selector
+  });
+
+  it('should go back to menu', () => {
+    // Type text into the textarea
+    const enteredText = 'Sample text for pasting';
+    cy.get('textarea').type(enteredText);
+
+    // Click the Paste button and navigate to the view page
+    cy.contains('button', 'Paste').click();
+    cy.url().should('include', '/view/'); // Navigate to the view page
+
+    // Verify if the entered text is displayed on the view page
+    cy.get('.paste-text').should('contain', enteredText); // Replace '.paste-text' with your actual selector
+    cy.get('.link').click();
+    cy.url().should('not.include', '/view/');
+  });
+
+  it('shoud show warning label if text is empty', () => {
+    cy.contains('button', 'Paste').click();
+    cy.get('.error-message').should('have.text', 'Please enter text to paste.');
+  });
+});
--- pastebin-frontend/src/PastingComponent.js
+++ pastebin-frontend/src/PastingComponent.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './PastingComponent.css'; // Import CSS file
+import './PastingComponent.css';
 
 const PastingComponent = () => {
   const [text, setText] = useState('');
